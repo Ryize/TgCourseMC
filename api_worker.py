2f6a2d137ee15d543d52e70c06821e6d9e3f0ad7
phoenixtheone <tm87rus67@gmail.com>
2024-05-11 18:44:52 +0300

    docs: добавлена документация к функциям welcome(),login(),password(),check_autorization() refactor: изменение имени модуля  работы с API docs: добавлена документация к get_data() в модуле работы с API docs: добавлена документация к клавиатурам

--- api_worker.py
+++ api_worker.py
@@ -12,7 +12,7 @@ STUDENT_API = os.getenv("STUDENT_API")
 
 def get_data():
     """
-    Функция получения донных из api
+    Функция получения донных из api.
     """
     data_no_json = requests.get(STUDENT_API).text
     data = json.loads(data_no_json)

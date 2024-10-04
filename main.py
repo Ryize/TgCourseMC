be3fc95489bc25430bd04b9d8651ebe8e4a7fff6
matvejcekasov <chekashovmatvey@gmail.com>
2024-10-04 20:53:16 +0300

    feat: Добавлена система автоматического перезапуска проекта раз в сутки

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 """
 Модуль запуска бота.
 """
+import multiprocessing
+import time
 
-if __name__ == "__main__":
+def main():
     import threading
 
     from bot import bot
-    from thread import get_pay, get_training, review
+    from thread import (get_pay, get_training, review,
+                        send_timetable_to_administrator,
+                        send_lesson_link_to_group)
 
     print('Бот запущен!')
     threading.Thread(target=get_training).start()
     threading.Thread(target=get_pay).start()
     threading.Thread(target=review).start()
+    threading.Thread(target=send_timetable_to_administrator).start()
+    threading.Thread(target=send_lesson_link_to_group).start()
+    bot.polling(non_stop=True)
+
+
+if __name__ == "__main__":
     while True:
-        try:
-            bot.infinity_polling()
-        except:
-            pass
+        process = multiprocessing.Process(target=main)
+        process.start()
+        time.sleep(86385)
+        process.kill()
+        time.sleep(15)

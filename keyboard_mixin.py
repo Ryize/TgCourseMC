01994fa4abe466568ba84df58353e45d4ad7ae46
matvejcekasov <chekashovmatvey@gmail.com>
2024-10-13 09:43:42 +0300

    feat: Добавлены новые клавиатуры для функционала собеса

--- keyboard_mixin.py
+++ keyboard_mixin.py
@@ -1,6 +1,7 @@
 """
 Модуль содержит клавиатуры, используемые в телеграм-боте.
 """
+
 from telebot import types
 
 
@@ -34,8 +35,9 @@ class KeyboardMixin:
         btn1 = types.KeyboardButton('Оплата 💰')
         btn2 = types.KeyboardButton('Пропустить занятие 💤')
         btn3 = types.KeyboardButton('Пинг ⚾')
+        btn4 = types.KeyboardButton('Твой собес 👨‍💻')
         kb.row(btn1, btn3)
-        kb.row(btn2)
+        kb.row(btn2, btn4)
         return kb
 
     @staticmethod
@@ -82,6 +84,108 @@ class KeyboardMixin:
         btn1 = types.KeyboardButton('Заявки 📝')
         btn2 = types.KeyboardButton('Отправить сообщение всем 📣')
         btn3 = types.KeyboardButton('Пинг ⚾')
+        btn4 = types.KeyboardButton('Твой собес 👨‍💻')
+        btn5 = types.KeyboardButton('Расписание 📆')
         kb.row(btn1, btn3)
-        kb.row(btn2)
+        kb.row(btn2, btn4)
+        kb.row(btn5)
+        return kb
+
+    @staticmethod
+    def check_category_interview() -> types.ReplyKeyboardMarkup:
+        """
+        Создает основную клавиатуру твой собес.
+
+        Returns:
+            types.ReplyKeyboardMarkup: Клавиатура с 4 кнопками
+        """
+        kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton('Вопросы')
+        btn2 = types.KeyboardButton('AI Собес')
+        kb.row(btn1, btn2)
+        return kb
+
+    @staticmethod
+    def category_kb() -> types.ReplyKeyboardMarkup:
+        """
+        Создает основную клавиатуру твой собес.
+
+        Returns:
+            types.ReplyKeyboardMarkup: Клавиатура с 4 кнопками
+        """
+        kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton('HR’ские')
+        btn2 = types.KeyboardButton('Python')
+        btn3 = types.KeyboardButton('ООП')
+        btn4 = types.KeyboardButton('Django')
+        kb.row(btn1, btn2)
+        kb.row(btn3, btn4)
+        return kb
+
+    @staticmethod
+    def difficulty_kb() -> types.ReplyKeyboardMarkup:
+        """
+        Создает основную клавиатуру твой собес.
+
+        Returns:
+            types.ReplyKeyboardMarkup: Клавиатура с 6 кнопками
+        """
+        kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton('1-3')
+        btn2 = types.KeyboardButton('1-5')
+        btn3 = types.KeyboardButton('1-7')
+        btn4 = types.KeyboardButton('3-5')
+        btn5 = types.KeyboardButton('5-7')
+        btn6 = types.KeyboardButton('7-9')
+        kb.row(btn1, btn2, btn3)
+        kb.row(btn4, btn5, btn6)
+        return kb
+
+    @staticmethod
+    def amount_question_kb() -> types.ReplyKeyboardMarkup:
+        """
+        Создает клавиатуру для выбора количества вопросов.
+
+        Returns:
+            types.ReplyKeyboardMarkup: Клавиатура с 4 кнопками
+        """
+        kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton('5')
+        btn2 = types.KeyboardButton('10')
+        kb.row(btn1, btn2)
+        return kb
+
+    @staticmethod
+    def weekday_buttons() -> types.ReplyKeyboardMarkup:
+        """
+        Создает клавиатуру для выбора количества вопросов.
+
+        Returns:
+            types.ReplyKeyboardMarkup: Клавиатура с 4 кнопками
+        """
+        kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton('Понедельник')
+        btn2 = types.KeyboardButton('Вторник')
+        btn3 = types.KeyboardButton('Среда')
+        btn4 = types.KeyboardButton('Четверг')
+        btn5 = types.KeyboardButton('Пятница')
+        btn6 = types.KeyboardButton('Суббота')
+        btn7 = types.KeyboardButton('Воскресенье')
+        kb.row(btn1, btn2, btn3)
+        kb.row(btn4, btn5, btn6)
+        kb.row(btn7)
+        return kb
+
+    @staticmethod
+    def next_ai_interview() -> types.InlineKeyboardMarkup:
+        """
+        Создает Inline клавиатуру для перехода к следующему вопросу.
+
+        Returns:
+            types.InlineKeyboardMarkup: Клавиатура с 1 кнопкой
+        """
+        kb = types.InlineKeyboardMarkup()
+        btn1 = types.InlineKeyboardButton(text='👉 Следующий вопрос',
+                                          callback_data='next_ai')
+        kb.row(btn1)
         return kb

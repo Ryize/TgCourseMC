db7bc0c889691e426b6fbc1aaebc0662c94e4385
matvejcekasov <chekashovmatvey@gmail.com>
2024-10-17 12:17:58 +0300

    feat: Реализованна поточная функция, для уведомления учеников и администратора о предстоящем уроке

--- thread.py
+++ thread.py
@@ -32,15 +32,18 @@ Telegram-боте, таких как проверка новых заявок н
     thread_payment = threading.Thread(target=get_pay)
     thread_payment.start()
 """
-
+import datetime
+import json
 import os
 import time
 
 import requests
 
-from api_worker import get_application, get_review
+from admin import send_admin_timetable, get_weekday
+from api_worker import get_application, get_review, get_weekday_timetable
 from billing import check_payment
 from bot import TG_ID_ADMIN, bot, pay_data
+from config import YANDEX_TOKEN
 from models import *
 
 TOKEN = os.getenv('TOKEN')
@@ -94,7 +97,7 @@ def get_pay():
         time.sleep(15)
         for i in pay_data.keys():
             if pay_data.get(i) and check_payment(
-                pay_data[i]['payment_id'], pay_data[i]['amount']
+                    pay_data[i]['payment_id'], pay_data[i]['amount']
             ):
                 bot.send_message(i, 'Ваш платеж успешно прошел!')
                 bot.send_message(
@@ -106,7 +109,8 @@ def get_pay():
                     f'на сумму: '
                     f'{pay_data[i]["amount"]}',
                 )
-                requests.post(PAYMENT_API + pay_data[i]['name'] + '/', timeout=5)
+                requests.post(PAYMENT_API + pay_data[i]['name'] + '/',
+                              timeout=5)
                 pay_data[i] = {}
 
 
@@ -134,3 +138,99 @@ def review():
                 rev = Review(id_review=i['id'])
 
                 rev.save()
+
+
+def send_timetable_to_administrator():
+    """
+    Функция каждую полночь отправляет Администратору сообщение о всех уроков
+    за текущий день.
+
+    Функция работает в бесконечном цикле.
+    """
+    while True:
+        now = datetime.datetime.now()
+        midnight = (now + datetime.timedelta(days=1)).replace(hour=0, minute=0,
+                                                              second=0,
+                                                              microsecond=0)
+        seconds_until_midnight = (midnight - now).total_seconds()
+
+        # Ждем до следующей полуночи
+        time.sleep(seconds_until_midnight)
+        send_admin_timetable()
+
+
+def check_time(target_time_str: str) -> bool:
+    """
+    Проверить время до начала урока в минутах.
+
+    Args:
+        target_time_str: str (время в формате 12:20)
+
+    Returns:
+        bool: True - если до урока от 1 до 5 минут, иначе False
+    """
+    # Получаем текущее время
+    now = datetime.datetime.now()
+
+    # Преобразуем строку времени в объект datetime
+    target_time = datetime.datetime.strptime(target_time_str, "%H:%M")
+
+    # Заменяем дату целевого времени на сегодняшнюю
+    target_time = target_time.replace(year=now.year, month=now.month, day=now.day)
+
+    # Вычисляем разницу во времени в минутах
+    diff = (target_time - now).total_seconds() / 60
+
+    # Проверяем, остаётся ли от 1 до 5 минут до целевого времени
+    if 1 <= diff <= 5:
+        return True
+    return False
+
+
+def get_lesson_link():
+    headers = {
+        'Authorization': YANDEX_TOKEN,
+        'Content-Type': 'application/json',
+    }
+
+    data = json.loads(requests.post(
+        'https://cloud-api.yandex.net/v1/telemost-api/conferences',
+        headers=headers).text)
+    return data['join_url']
+
+
+def send_lesson_link_to_group():
+    """
+    Функция отправляет ссылку на урок в телемосте за 5 минут до начала занятия.
+
+    Отправляет всем в учебной группе и администратору.
+    """
+    while True:
+        weekday = get_weekday()
+        data = get_weekday_timetable(weekday)
+
+        group_id = None
+        for i in data['timetables']:
+            if check_time(i['time_lesson'][:5]):
+                for group in json.loads(
+                        requests.get('https://coursemc.ru/api/v1/groups/').text
+                ):
+                    if group['title'] == i['group']:
+                        group_id = group['id']
+        if not group_id:
+            time.sleep(60)
+            continue
+
+        lesson_url = get_lesson_link()
+
+        bot.send_message(TG_ID_ADMIN, f'👉 Ссылка на урок: {lesson_url}')
+
+        for student in json.loads(
+                requests.get('https://coursemc.ru/api/v1/student/').text
+        ):
+            if student['groups'] == group_id:
+                user = User.select().where(
+                    User.name == student['name']).first()
+                if user:
+                    bot.send_message(int(user.chat_id), f'👉 Ссылка на урок: {lesson_url}')
+        time.sleep(900)
